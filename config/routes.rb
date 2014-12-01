dbaf3fb0d2b1bedc9eb8ac5fe67a494db8223749
taith <taisvbk@gmail.com>
2014-12-01 17:53:01 +0700

    Finish login-logout

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 Rails.application.routes.draw do
+  get 'sessions/new'
+
   get 'contact' => 'static_pages#contact'
 
   get 'about' =>  'static_pages#about'
@@ -8,6 +10,9 @@ Rails.application.routes.draw do
   get 'help'  =>  'static_pages#help'
   
   get 'signup'  => 'users#new'
+  get 'login' =>  'sessions#new'
+  post  'login' =>  'sessions#create'
+  delete  'logout'  =>  'sessions#destroy'   
   
   resources :users
   # The priority is based upon order of creation: first created -> highest priority.
--- test/models/user_test.rb
+++ test/models/user_test.rb
@@ -29,16 +29,19 @@ class UserTest < ActiveSupport::TestCase
     end
   end
   
-    test "email addresses should be unique" do
+  test "email addresses should be unique" do
     duplicate_user = @user.dup
     duplicate_user.email = @user.email.upcase
     @user.save
     assert_not duplicate_user.valid?
   end
   
-    test "password should have a minimum length" do
+  test "password should have a minimum length" do
     @user.password = @user.password_confirmation = "a" * 5
     assert_not @user.valid?
   end
-
+  
+  test "authenticated? should return false for a user with nil digest" do
+    assert_not @user.authenticated?('')
+  end
 end
